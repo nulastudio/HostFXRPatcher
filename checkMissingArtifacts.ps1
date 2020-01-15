0183917e883b89f434a3d20a406b3746c20a875b
L!εsAμεr <liesauer@liesauer.net>
2020-01-15 17:54:38 +0800

    upload checkMissingArtifacts.ps1

---
+++ checkMissingArtifacts.ps1
@@ -0,0 +1,26 @@
+$rootdir = $pwd
+$artifactsdir = "${rootdir}/artifacts"
+
+
+if (!(Test-Path "${artifactsdir}")) {
+    Write-Host "no artifacts found"
+    exit
+}
+
+$arches = "win-x86", "win-x64", "linux-x64", "osx-x64"
+
+function Check-Arch($path) {
+    $segment = $path.Split("/")
+    $version = $segment[($segment.Count - 1)]
+    foreach ($arch in $arches) {
+        if (!(Test-Path "${path}/${arch}.Release")) {
+            Write-Host "${version} does not contain arch ${arch}"
+        }
+    }
+}
+
+Get-ChildItem $artifactsdir | ForEach-Object -Process {
+    if ($_.psiscontainer) {
+        Check-Arch -Path "${artifactsdir}/$($_.Name)"
+    }
+}
