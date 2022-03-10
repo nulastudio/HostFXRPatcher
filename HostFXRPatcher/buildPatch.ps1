803d8a798fec374ff54d1bf650e8f383fd4bd506
L!εsAμεr <liesauer@liesauer.net>
2022-03-11 00:24:58 +0800

    Support .NET 7

--- HostFXRPatcher/buildPatch.ps1
+++ HostFXRPatcher/buildPatch.ps1
@@ -231,7 +231,7 @@ if (Test-Path("${scriptdir}/VersionReleased.json")) {
 foreach ($tag in (git tag))
 {
     # 只编译2.x+版本
-    if (($tag -like "v2*") -or ($tag -like "v3*") -or ($tag -like "v5*") -or ($tag -like "v6*")) {
+    if (($tag -like "v2*") -or ($tag -like "v3*") -or ($tag -like "v5*") -or ($tag -like "v6*") -or ($tag -like "v7*")) {
         # 2.x版本只编译正式版
         if (($tag -like "v2*") -and ($tag -match "[^v\d\.]")) {
             continue
