edcf5c611fdcd7c1fe32f3d1935a17a2f80db70a
L!εsAμεr <liesauer@liesauer.net>
2024-01-10 22:21:26 +0800

    Add osx-arm64 support

--- minifyCompatibilityJSON.ps1
+++ minifyCompatibilityJSON.ps1
@@ -18,16 +18,56 @@ function Sort-Dict($Dict) {
     return $hashtable
 }
 
-$compatibilityJsonUrl = "https://raw.githubusercontent.com/dotnet/runtime/main/src/libraries/Microsoft.NETCore.Platforms/src/runtime.compatibility.json"
+function Resolve-Compatibility-Path($Rid, $Runtimes, $level = 1, $index = 0) {
+    [System.Collections.ArrayList]$result = @()
 
-$compatibilityJson = (Get-NetworkString $compatibilityJsonUrl) | ConvertFrom-Json
+    if ($Runtimes.Contains($Rid) && $Runtimes.$Rid.Contains("#import")) {
+        $map = $Runtimes.$Rid."#import"
+
+        foreach($path in $map)
+        {
+            $index++
+            [void]$result.Add(@{"index" = $index; "level" = $level; "rid" = $path})
+
+            $_all = Resolve-Compatibility-Path $path $Runtimes ($level+1) ($index)
+
+            foreach($_path in $_all)
+            {
+                $index = $_path."index"
+                [void]$result.Add($_path)
+            }
+        }
+    }
+
+    return $result
+}
+
+function Resolve-Compatibility-Array($Rid, $Runtimes) {
+    $result = Resolve-Compatibility-Path $Rid $Runtimes | Sort-Object -Property @{Expression = "level"; Descending = $false},@{Expression = "index"; Descending = $false}
+
+    [System.Collections.ArrayList]$all = @($Rid)
+
+    foreach($rid in $result)
+    {
+        if (!$all.Contains($rid."rid")) {
+            [void]$all.Add($rid."rid")
+        }
+    }
+
+    return $all
+}
+
+$compatibilityJsonUrl = "https://raw.githubusercontent.com/dotnet/runtime/main/src/libraries/Microsoft.NETCore.Platforms/src/runtime.json"
+
+$compatibilityJson = (Get-NetworkString $compatibilityJsonUrl) | ConvertFrom-Json -AsHashTable
 $supportedJson = (Get-Content $supportedpath) | ConvertFrom-Json
 
 $minifyCompatibilityJson = @{}
 
-foreach($runtime in $compatibilityJson.psobject.properties.name)
+foreach($runtime in $compatibilityJson."runtimes".GetEnumerator())
 {
-    $runtimes = $compatibilityJson.$runtime
+    $runtimes = Resolve-Compatibility-Array $runtime.Key $compatibilityJson."runtimes"
+
     [System.Collections.ArrayList]$minifyRuntimes = @()
     foreach($supportedRuntime in $supportedJson)
     {
@@ -38,7 +78,7 @@ foreach($runtime in $compatibilityJson.psobject.properties.name)
         }
     }
     if ($minifyRuntimes.Length) {
-        $minifyCompatibilityJson[$runtime] = $minifyRuntimes
+        $minifyCompatibilityJson[$runtime.Key] = $minifyRuntimes
     }
 }
 
